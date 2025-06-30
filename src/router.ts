802b2927cd379220f5596565efb3938a8348c678
Muhammad <zoirovm823@gmail.com>
2025-06-30 20:43:49 +0500

    adsds

--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,7 @@ router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
 router.get("/member/detail", memberController.verifyAuth); 
 
-
+console.log("dxca")
 
 /** Product**/
 
