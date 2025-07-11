204c82ab44015424a4c2898b7f03660cb607d4ab
Muhammad <zoirovm823@gmail.com>
2025-07-12 01:38:06 +0500

    feat: develop createOrder rest Api

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
 import productController from "./controllers/product.controller";
+import orderController from "./controllers/order.controller";
 
 /** Member**/
 router.get("/member/restaurant", memberController.getRestaurant);
@@ -21,16 +22,23 @@ router.get(
 router.post(
   "/member/update",
   memberController.verifyAuth,
-  uploader("members").single("memberImage"), 
+  uploader("members").single("memberImage"),
   memberController.updateMember
 );
-router.get("/member/top-users" , memberController.getTopUsers)
+router.get("/member/top-users", memberController.getTopUsers);
 
 /** Product**/
 router.get("/product/all", productController.getProducts);
-router.get("/product/:id", memberController.retriveAuth, productController.getProduct)
-
+router.get(
+  "/product/:id",
+  memberController.retriveAuth,
+  productController.getProduct
+);
 
 /** Order**/
-
+router.post(
+  "/order/create",
+  memberController.verifyAuth,
+  orderController.createOrder
+);
 export default router;
