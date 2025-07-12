b2b964b0eb2d23ba4b10675a11fc78a479ac013b
Muhammad <zoirovm823@gmail.com>
2025-07-13 00:19:41 +0500

    feat: develop updateOrder rest Api

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -2,8 +2,8 @@ import { ExtendedRequest } from "../libs/types/member";
 import { T } from "../libs/types/common";
 import { Response } from "express";
 import Errors, { HttpCode } from "../libs/Errors";
-import OrderService from "../models/Order.service"
-import { OrderInquiry } from "../libs/types/order";
+import OrderService from "../models/Order.service";
+import { OrderInquiry, OrderUpdateInput } from "../libs/types/order";
 import { OrderStatus } from "../libs/enums/order.enum";
 
 const orderService = new OrderService();
@@ -13,7 +13,7 @@ orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
   try {
     console.log("createOrder");
     const result = await orderService.createOrder(req.member, req.body);
-    res.status(HttpCode.CREATED).json(result)
+    res.status(HttpCode.CREATED).json(result);
   } catch (err) {
     console.log("Error, createOrder", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -22,26 +22,38 @@ orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
 };
 
 orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
-    try {
-      console.log("getMyOrders"); 
-      const {page, limit, orderStatus}= req.query
+  try {
+    console.log("getMyOrders");
+    const { page, limit, orderStatus } = req.query;
 
-      const inquiry: OrderInquiry = {
-        page: Number(page),
-        limit:Number(limit),
-        orderStatus: orderStatus as OrderStatus
-        
-      };
-      console.log("inqury", inquiry)
-      const result = await orderService.getMyOrders(req.member, inquiry)
+    const inquiry: OrderInquiry = {
+      page: Number(page),
+      limit: Number(limit),
+      orderStatus: orderStatus as OrderStatus,
+    };
+    console.log("inqury", inquiry);
+    const result = await orderService.getMyOrders(req.member, inquiry);
 
+    res.status(HttpCode.CREATED).json(result);
+  } catch (err) {
+    console.log("Error, getMyOrders", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 
-      res.status(HttpCode.CREATED).json(result)
-    } catch (err) {
-      console.log("Error, getMyOrders", err);
-      if (err instanceof Errors) res.status(err.code).json(err);
-      else res.status(Errors.standard.code).json(Errors.standard);
-    }
-  };
+orderController.updateOrder = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("updateOrder");
+    const input: OrderUpdateInput = req.body; 
+    const result = await orderService.updateOrder(req.member, input);
+
+    res.status(HttpCode.CREATED).json(result);
+  } catch (err) {
+    console.log("Error, updateOrder", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 
 export default orderController;
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -16,19 +16,15 @@ class MemberService {
   constructor() {
     this.memberModel = MemberModel;
   }
-  /* SPA*/ 
-
-  
-  
+  /* SPA*/
 
   public async getRestaurant(): Promise<Member> {
     const result = await this.memberModel
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
-      if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
-      return result;
-
+    return result;
   }
 
   public async signup(input: MemberInput): Promise<Member> {
@@ -103,6 +99,20 @@ class MemberService {
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
     return result;
+  }
+
+  public async addUserPoint(member: Member, point: Number): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+
+    return await this.memberModel.findOneAndUpdate({
+      _id: memberId,
+      memberType: MemberType.USER,
+      memberStatus: MemberStatus.ACTIVE,
+    },
+    {$inc: {memberPoints: point}},
+    {new: true}
+  )
+  .exec();
   }
   /* SSR*/
 
